ee31d7bab1a61acad5ae5c1f50e319d212f56140
ubahhukob@yandex.ru <ubahhukob@ya.ru>
2021-10-15 00:07:52 +0500

    first commit

---
+++ src/main/java/jentus/vocabulary/config/WebConfig.java
@@ -0,0 +1,14 @@
+package jentus.vocabulary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
